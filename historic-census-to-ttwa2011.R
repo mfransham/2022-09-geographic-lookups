ee51a94876d7ed3df61fb86f5bcc294b0e934a0b
Mark Fransham <Mark.Fransham@apsoc.ox.ac.uk>
2022-09-21 17:56:03 +0100

    completed lookup table from ward 1981

--- historic-census-to-ttwa2011.R
+++ historic-census-to-ttwa2011.R
@@ -14,17 +14,21 @@ library(mapview)
 # from https://geoportal.statistics.gov.uk/search?collection=Dataset&sort=name&tags=all(BDY_TTWA%2CDEC_2011)
 ttwa2011 <- st_read("../../Data/Boundaries/Travel_to_Work_Areas_December_2011_Generalised_Clipped_Boundaries_in_United_Kingdom")
 ttwa2011 %>% st_geometry() %>% plot()
-# mapview(ttwa2011["ttwa11nm"], fgb = FALSE)
+# mapview(ttwa2011)
 
 # local authority districts (363, as at April 2021)
 # from https://geoportal.statistics.gov.uk/search?collection=Dataset&sort=name&tags=all(BDY_LAD%2CDEC_2021)
 lad2021 <- st_read("../../Data/Boundaries/Local_Authority_Districts_(December_2021)_GB_BGC")
 
+# English region and country 
+# from https://geoportal.statistics.gov.uk/search?collection=Dataset&sort=name&tags=all(BDY_ITL1%2CJAN_2021)
+region <- st_read("../../Data/Boundaries/International_Territorial_Level_1_(January_2021)_UK_BGC")
 
-# 1981 Census geographies --------------------------------------------------------------
+
+# 1981 Census geographies --------------------------------------------------------------------------------
 
 # these boundaries downloaded from https://borders.ukdataservice.ac.uk/easy_download.html
-# England and Wales are wards; Scotland is actually enumeration districts or postcode sectors
+# England and Wales are wards; Scotland is actually postcode sectors
 ward_eng_81 <- st_read("../../Data/Boundaries/Wards historic/England_wa_1981")
 ward_sco_81 <- st_read("../../Data/Boundaries/Wards historic/Scotland_wa_1981")
 ward_wal_81 <- st_read("../../Data/Boundaries/Wards historic/Wales_wa_1981")
@@ -33,7 +37,7 @@ ward_wal_81 <- st_read("../../Data/Boundaries/Wards historic/Wales_wa_1981")
 ward_gb_81 <- bind_rows(ward_eng_81, ward_sco_81, ward_wal_81)
 rm(ward_eng_81, ward_sco_81, ward_wal_81)
 
-# spatial join of LAD 2021
+# spatial join of LAD 2021 ----------------
 ward_gb_81 <- 
   ward_gb_81 %>% 
   st_join(lad2021 %>% select(LAD21CD, LAD21NM, LAD21NMW), 
@@ -51,7 +55,7 @@ ward_gb_81 <- ward_gb_81 %>%
   mutate(LAD21CD = ifelse(label == "6554AC", "S12000023", LAD21CD), 
          LAD21NM = ifelse(label == "6554AC", "Orkney Islands", LAD21NM) )
 
-# spatial join of TTWA 2011
+# spatial join of TTWA 2011 ------------------------
 ward_gb_81 <- 
   ward_gb_81 %>% 
   st_join(ttwa2011 %>% select(ttwa11cd, ttwa11nm), 
@@ -61,22 +65,72 @@ ward_gb_81 <-
 # check for any non-matching areas 
 ward_gb_81 %>% filter(is.na(ttwa11cd) )
 # view on map
-mapview(ttwa2011) + 
-  ward_gb_81 %>% filter(label == "6332CE") %>% mapview()
+mapview(ttwa2011) +
+  ward_gb_81 %>% filter(label == "35NJAN") %>% mapview()
 # join with nearest feature 
-ward_gb_81 %>% 
+non_matching <- 
+  ward_gb_81 %>% 
   filter(is.na(ttwa11cd) ) %>% 
   select(-ttwa11cd, -ttwa11nm) %>% 
   st_join(ttwa2011 %>% select(ttwa11cd, ttwa11nm), 
           join = st_nearest_feature)
+# remove incomplete records, add complete records
+ward_gb_81 <- 
+  ward_gb_81 %>% 
+  filter(!(is.na(ttwa11cd) ) ) %>% 
+  bind_rows(non_matching)
+# remove auxiliary data frame
+rm(non_matching)
 
+# spatial join of region ----------------------------------------------
+ward_gb_81 <- 
+  ward_gb_81 %>% 
+  st_join(region %>% select(ITL121CD, ITL121NM), 
+          join = st_intersects, 
+          largest = T)
+
+# check for any non-matching areas 
+ward_gb_81 %>% filter(is.na(ITL121CD) )
+# join with nearest feature 
+non_matching <- 
+  ward_gb_81 %>% 
+  filter(is.na(ITL121CD) ) %>% 
+  select(-ITL121CD, -ITL121NM) %>% 
+  st_join(region %>% select(ITL121CD, ITL121NM), 
+          join = st_nearest_feature)
+# remove incomplete records, add complete records  
+ward_gb_81 <- 
+  ward_gb_81 %>% 
+  filter(!(is.na(ITL121CD) ) ) %>% 
+  bind_rows(non_matching)
+# remove auxiliary data frame
+rm(non_matching)
+
+# create lookup table --------------------------------------------
+lkup_ward81_ttwa2011_lad2021 <- 
+  ward_gb_81 %>% 
+  as.data.frame() %>% 
+  select(-geometry) %>% 
+  group_by(label) %>% 
+  slice_sample(n = 1) %>% 
+  ungroup()
+
+# check against 1981 bulk data
+# this works - the only missing codes are those for people at sea (codes ending in 'SS')
+# census81 <- read_csv("../../Data/Census 1981/England Wales Scotland Small Area Statistics/81sas05ews_3.csv") %>% 
+#   select(zoneid, `81sas050380`) %>% 
+#   left_join(lkup_ward81_ttwa2011_lad2021, 
+#             by = c("zoneid" = "label") )
+# census81 %>% filter(is.na(ttwa11cd) ) %>% mutate(ss = substr(zoneid, 5, 6) ) %>% View()
+
+# write out lookup table 
+write_csv(lkup_ward81_ttwa2011_lad2021, "lookup-tables/lkup_ward81_ttwa2011_lad2021.csv")
+
+
+# 1991 Census geographies --------------------------------------------------------------------------------
 
 
 ########## TNT ##########################
-# join these complete records to the main table 
-# add region 
-# create lookup table, making sure only one entry per ward / postcode sector
-# check this lookup table matches with the bulk data downloaded for 1981 Census 
 # then move on to 1991, 2001, 2011
 # later: could do the same for NESPD areas, should be pretty easy if this works (as it seems to be doing) 
 
