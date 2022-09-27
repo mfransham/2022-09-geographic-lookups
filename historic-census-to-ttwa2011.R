892f93b163c12df3e2b9281ce6dd1fdd52df2b38
Mark Fransham <Mark.Fransham@apsoc.ox.ac.uk>
2022-09-27 10:51:03 +0100

    remove manual coding of nearest feature

--- historic-census-to-ttwa2011.R
+++ historic-census-to-ttwa2011.R
@@ -46,14 +46,20 @@ ward_gb_81 <-
 
 # check for any non-matching areas 
 ward_gb_81 %>% filter(is.na(LAD21CD) )
-# one shapefile with no LAD match
-ward_gb_81 %>% filter(label == "6554AC") %>% mapview(label = "LAD21NM")
-# belongs to Orkney Islands
-ward_gb_81 %>% filter(label == "6554AC") %>% View()
-# add manually
-ward_gb_81 <- ward_gb_81 %>% 
-  mutate(LAD21CD = ifelse(label == "6554AC", "S12000023", LAD21CD), 
-         LAD21NM = ifelse(label == "6554AC", "Orkney Islands", LAD21NM) )
+# join with nearest feature 
+non_matching <- 
+  ward_gb_81 %>% 
+  filter(is.na(LAD21CD) ) %>% 
+  select(-LAD21CD, -LAD21NM, -LAD21NMW) %>% 
+  st_join(lad2021 %>% select(LAD21CD, LAD21NM, LAD21NMW), 
+          join = st_nearest_feature)
+# remove incomplete records, add complete records
+ward_gb_81 <- 
+  ward_gb_81 %>% 
+  filter(!(is.na(LAD21CD) ) ) %>% 
+  bind_rows(non_matching)
+# remove auxiliary data frame
+rm(non_matching)
 
 # spatial join of TTWA 2011 ------------------------
 ward_gb_81 <- 
